ab584601ce9d11e7c485a5cb1883a91ed69240aa
Miro Benicio <mirodantas27@gmail.com>
2022-12-14 19:17:18 -0300

    blog application

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="grid h-screen place-content-center gap-3">
+    <h1 class="text-5xl text-blue-500">Bem vindo ao Nuxt Content</h1>
+    <p class="text-xl text-blue-600">
+      Dê uma olhada
+      <a href="https://content.nuxtjs.org/">na documentação aqui</a>
+    </p>
+    <nuxt-link
+      href="/blog"
+      class="mx-auto rounded-xl bg-blue-300 px-20 py-4 text-white"
+    >
+      Blog
+    </nuxt-link>
+  </div>
+</template>
